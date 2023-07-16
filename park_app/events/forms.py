56333047d033ecd362435f3e6a34ec24ad3986ab
JacobFleischauer7 <126291602+JacobFleischauer7@users.noreply.github.com>
2023-07-16 17:00:44 -0400

    forms.py

--- park_app/events/forms.py
+++ park_app/events/forms.py
@@ -18,7 +18,9 @@ class VenueForm(ModelForm):
 
 
 # Add event form
-class EventForm(ModelForm):
+
+# Admin SuperUser event form
+class EventFormAdmin(ModelForm):
 	class Meta:
 		model = Event
 		fields = ('name', 'timeslot', 'venue', 'host', 'attendees', 'description')
@@ -35,6 +37,24 @@ class EventForm(ModelForm):
 			'timeslot': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Date'}),
 			'venue': forms.Select(attrs={'class':'form-select', 'placeholder':'Venue'}),
 			'host': forms.Select(attrs={'class':'form-select', 'placeholder':'Host'}),
+			'attendees': forms.SelectMultiple(attrs={'class':'form-control', 'placeholder':'Attendees'}),
+			'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description'}),
+# User event form			
+class EventForm(ModelForm):
+	class Meta:
+		model = Event
+		fields = ('name', 'timeslot', 'venue','attendees', 'description')
+		labels = {
+			'name': '',
+			'timeslot': 'YYYY-MM-DD HH:MM:SS',
+			'venue': 'Venue',
+			'attendees': 'Attendees',
+			'description': '',
+		}
+		widgets = {
+			'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Name'}),
+			'timeslot': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Date'}),
+			'venue': forms.Select(attrs={'class':'form-select', 'placeholder':'Venue'}),
 			'attendees': forms.SelectMultiple(attrs={'class':'form-control', 'placeholder':'Attendees'}),
 			'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description'}),
 		}
