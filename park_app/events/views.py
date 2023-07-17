aa79e792d033d1d40ec91cb17ce14b36583b0cad
JacobFleischauer7 <126291602+JacobFleischauer7@users.noreply.github.com>
2023-07-17 13:37:56 -0400

    views.py

--- park_app/events/views.py
+++ park_app/events/views.py
@@ -108,7 +108,7 @@ def search_venues(request):
 def show_venue(request, venue_id):
 	venue = Venue.objects.get(pk=venue_id)
 	venue_owner = User.objects.get(pk=venue.owner)
-	return render(request, 'events/show_venue.html', {'venue': venue})
+	return render(request, 'events/show_venue.html', {'venue': venue,'venue_owner':venue_owner})
 
 
 def list_venues(request):
