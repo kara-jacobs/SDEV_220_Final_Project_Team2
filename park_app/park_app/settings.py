d502e70bf1fd23446f8a90ffccf0ad880b86ed4f
Kara Jacobs <kjacobs67@ivytech.edu>
2023-07-27 21:58:14 -0400

    Added some more pics to the app.

--- park_app/park_app/settings.py
+++ park_app/park_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+0e+i=+mpwj(to#cbu65cpbiy#1ok-na5q!czquoban@m_p+*l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
