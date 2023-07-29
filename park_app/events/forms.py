a882d1f605c99376f28d6b0adc9bbede0dfda691
Kara Jacobs <kjacobs67@ivytech.edu>
2023-07-28 20:09:18 -0400

    Removed from forms, added RSVP buttons to My Events.

--- park_app/events/forms.py
+++ park_app/events/forms.py
@@ -22,13 +22,12 @@ class VenueForm(ModelForm):
 class EventFormAdmin(ModelForm):
 	class Meta:
 		model = Event
-		fields = ('name', 'timeslot', 'venue', 'host', 'attendees', 'description', 'event_image')
+		fields = ('name', 'timeslot', 'venue', 'host', 'description', 'event_image')
 		labels = {
 			'name': '',
 			'timeslot': 'YYYY-MM-DD HH:MM:SS',
 			'venue': 'Venue',
 			'host': 'Host',
-			'attendees': 'Attendees',
 			'description': '',
 			'event_image': '',
 		}
@@ -37,7 +36,6 @@ class EventFormAdmin(ModelForm):
 			'timeslot': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Date'}),
 			'venue': forms.Select(attrs={'class':'form-select', 'placeholder':'Venue'}),
 			'host': forms.Select(attrs={'class':'form-select', 'placeholder':'Host'}),
-			'attendees': forms.SelectMultiple(attrs={'class':'form-control', 'placeholder':'Attendees'}),
 			'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description'}),
 		}
 
@@ -46,12 +44,11 @@ class EventFormAdmin(ModelForm):
 class EventForm(ModelForm):
 	class Meta:
 		model = Event
-		fields = ('name', 'timeslot', 'venue', 'attendees', 'description', 'event_image')
+		fields = ('name', 'timeslot', 'venue', 'description', 'event_image')
 		labels = {
 			'name': '',
 			'timeslot': 'YYYY-MM-DD HH:MM:SS',
 			'venue': 'Venue',
-			'attendees': 'Attendees',
 			'description': '',
 			'event_image': '',
 		}
@@ -59,6 +56,5 @@ class EventForm(ModelForm):
 			'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Name'}),
 			'timeslot': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Date'}),
 			'venue': forms.Select(attrs={'class':'form-select', 'placeholder':'Venue'}),
-			'attendees': forms.SelectMultiple(attrs={'class':'form-control', 'placeholder':'Attendees'}),
 			'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description'}),
 		}
